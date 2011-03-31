7fbb2100f8e27278b4ab42e8cf172ce8ce852e74
unknown <mattpisgod@hotmail.com>
2011-03-30 23:53:54 -0800

    Account Javadoc

---
+++ Account.java
@@ -0,0 +1,163 @@
+/**
+ *	Account Class
+ *
+ *      Stores basic information of an account
+ *      @author mattp
+ *	@version 1.0 March 30, 2011
+ */
+public class Account
+{
+    /**
+     * Account default constructor
+     * @param accountID the account ID of the user
+     * @param Fname the first name of the user
+     * @param Lname the last name of the user
+     * @param address the address of the user
+     * @param phoneNum the phone number of the user
+     */
+    public Account(int accountID, String Fname, String Lname, String address, String phoneNum)
+    {
+        this.accountID = accountID;
+        this.Fname = Fname;
+        this.Lname = Lname;
+        this.address = address;
+        this.phoneNum = phoneNum;
+        status = "Active";
+    }
+    /**
+     * Set log in information
+     * @param loginID the log in ID/username of the account
+     * @param password the password of the account
+     */
+    public void setLogin(String loginID, String password)
+    {
+        setPassword(password);
+        setLoginID(loginID);
+    }
+    /**
+     * Set personal information 
+     * @param Fname the first name of the user
+     * @param Lname the last name of the user
+     * @param address the address of the user
+     * @param phoneNum the phone number of the user
+     */
+    public void setPersonalInfo(String Fname, String Lname, String address, String phoneNum)
+    {
+        setFname(Fname);
+        setLname(Lname);
+        setAddress(address);
+        setPhoneNum(phoneNum);
+    }
+    /**
+     * Set status of the account 
+     * @param status the status of the account(Active/Inactive)
+     */
+    public void setStatus(String status)
+    {
+        this.status = status;
+    }
+    /**
+     * Set password
+     * @param password the password of the account
+     */
+    protected void setPassword(String password)
+    {
+        this.password = password;
+    }
+    /**
+     * Set login ID
+     * @param loginID the log in ID/username of the account
+     */
+    protected void setLoginID(String loginID)
+    {
+        this.loginID = loginID;
+    }
+    /**
+     * Set first name
+     * @param Fname the first name of the user
+     */
+    protected void setFname(String Fname)
+    {
+        this.Fname = Fname;
+    }
+    /**
+     * Set last name
+     * @param Lname the last name of the user
+     */
+    protected void setLname(String Lname)
+    {
+        this.Lname = Lname;
+    }
+    /**
+     * Set address
+     * @param address the address of the user
+     */
+    protected void setAddress(String address)
+    {
+        this.address = address;
+    }
+    /**
+     * Set phone number
+     * @param phoneNum the phone number of the user
+     */
+    protected void setPhoneNum(String phoneNum)
+    {
+        this.phoneNum = phoneNum;
+    }
+    /**
+     * Get account ID
+     * @return phoneNum the account ID of the account
+     */
+    public int getAcccountID()
+    {
+        return accountID;
+    }
+    /**
+     * Get first name
+     * @return Fname the first name of the user
+     */
+    public String getFname()
+    {
+        return Fname;
+    }
+    /**
+     * Get last name
+     * @return Lname the last name of the user
+     */
+    public String getLname()
+    {
+        return Lname;
+    }
+    /**
+     * Get address
+     * @return address the address of the user
+     */
+    public String getAddress()
+    {
+        return address;
+    }
+    /**
+     * Get password
+     * @return password the password of the account
+     */
+    public String getPassword()
+    {
+        return password;
+    }
+    /**
+     * Get status
+     * @return status the status of the account (Active/Inactive)
+     */
+    public String getStatus()
+    {
+        return status;
+    }
+    private int accountID;
+    private String loginID;
+    private String Fname;
+    private String Lname;
+    private String phoneNum;
+    private String address;
+    private String password;
+    private String status;
+}
---
+++ Employee.java
@@ -0,0 +1,51 @@
+/**
+ *	Employee Class
+ *      subclass of Account class
+ *
+ *      Stores information of an employee account
+ *      @author mattp
+ *	@version 1.0 March 30, 2011
+ */
+public class Employee extends Account
+{
+    /**
+     * Default constructor of Employee
+     * @param position the position of the employee (Manager/Staff)
+     * @param accountID the account ID of the account
+     * @param Fname the first name of the user
+     * @param Lname the last name of the user
+     * @param address the address of the user
+     * @param phoneNum the phone number of the user
+     */
+    public Employee (String position, int accountID, String Fname, String Lname, String address, String phoneNum)
+    {
+        super(accountID, Fname, Lname, address,phoneNum);
+        this.position = position;
+    }
+    /**
+     * Get position
+     * @return position the position of the employee
+     */
+    public String getPosition()
+    {
+        return position;
+    }
+    /**
+     * Set position
+     * @param position the position of the employee
+     */
+    public void setPosition(String position)
+    {
+        this.position = position;
+    }
+    /**
+     * Check if the employee is a manager
+     * @return boolean the value shows this employee is a manager or not
+     */
+    public boolean isManager()
+    {
+       return position.equals("Manager");
+    }
+
+    private String position;
+}
