e3f5679348c059914aad980fbfb13d49d16859ba
pc <petchu@gmail.com>
2011-04-02 10:00:19 -0700

    added updateItemInfo() method

--- TransactionItem.java
+++ TransactionItem.java
@@ -1,16 +1,30 @@
-// temp stub class made by peter to test Transaction class
-
+/**
+ * This is a inventory item that the store carries with it's own unique barcode number.
+ */
 public interface TransactionItem
 {
-/*
-	public TransactionItem(String type, String name, int price)
-	{
-		this.type = type;
-		this.name = name;
-		this.price = price;
-	}
-*/
+	/**
+		This gets the type of the item (Rental Video, Sale Video, Promo, Penalty, etc).
+		@return the item's type is returned.
+	*/
 	String getType();
+	/**
+		This gets the name of the item.
+		@return the name of the item.
+	*/
 	String getName();
+	/**
+		The price of the item (can be negative for discounts)
+		@return the price of the item in cents
+	*/
 	int getPrice(); // price in cents
-}
+	/**
+		The item's barcode number
+		@return the barcode of the item
+	*/
+	String getBarcode();
+	/**
+	 * This updates the item info (to be used when the item is added to the Transaction, example: for a Rental Video, the video should be marked as checked by the customer on the given date, and for Penalties  it should be marked as paid, and for Promos maybe nothing or for future functions)
+	 * @param 
+	 */
+	boolean updateItemInfo(Customer customerID, Date theDate);
