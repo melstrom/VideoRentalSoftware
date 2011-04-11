8d104f98a88305ff2e182b879502d14d2f9e8b00
Cary Wong <melstrom@gmail.com>
2011-04-11 12:59:12 -0700

    Added proxy skeletons

---
+++ inventory/InventoryProxy.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package inventory;
+
+/**
+ *
+ * @author melstrom
+ */
+public class InventoryProxy {
+    public void addMovieRequest()
+    {
+
+    }
+
+    public void reserveMovie()
+    {
+        
+    }
+}
---
+++ search/SearchProxy.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package search;
+
+/**
+ *
+ * @author melstrom
+ */
+public class SearchProxy {
+        public void searchVideo()
+    {
+
+        }
+/**
+ * <p>Does ...</p>
+ *
+ * @poseidon-object-id [Im68939bd3m12f379ec5cemm615b]
+ */
+    public void previewVideo()
+    {
+
+    }
+/**
+ * <p>Does ...</p>
+ *
+ * @poseidon-object-id [Im68939bd3m12f379ec5cemm6136]
+ */
+    public void browse()
+    {
+        
+    }
+
+}
