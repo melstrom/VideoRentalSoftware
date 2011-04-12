5c4909972d2b150d9101c41184aa8383710199e3
unknown <Administrator@.(none)>
2011-04-11 19:45:43 -0700

    Minor changes and cleanup

--- account/AccountManagement.java
+++ account/AccountManagement.java
@@ -1,5 +1,6 @@
 package account;
 // TODO: sepearate account attribute into employee and customer
+
 /**
  * Account Manager class
  * 
@@ -12,24 +13,20 @@ package account;
  *	-added JDBCConnetion package
  *	-fixed demote(), promote() and editAccount()
  */
-import java.sql.Connection;
-import java.sql.Statement;
-//import java.sql.DriverManager;
-import jdbconnection.*;
 import java.sql.SQLException;
+import java.sql.Connection;
 import jdbconnection.JDBCConnection;
 
-public class AccountManagement
+public class AccountManagement {
 
-{
     /**
      * Default constructor with no parameters
      */
-    public AccountManagement()throws SQLException, ClassNotFoundException
-    {
-	JDBC = new JDBCConnection();
-      connection = JDBC.getConnection();
+    public AccountManagement() throws SQLException, ClassNotFoundException {
+        JDBC = new JDBCConnection();
+        connection = JDBCConnection.getConnection();
     }
+
     /**
      * Create an employee account
      * @param position the position of the employee (Manager/Staff)
@@ -40,11 +37,12 @@ public class AccountManagement
      * @param phoneNum the phone number of the user
      */
     public void createEmployee(String position, int accountID, String Fname, String Lname, String address, String phoneNum)
-            throws SQLException,java.lang.Exception
-    {
-        if(!isDuplicatedID(accountID, "employee"))
-        employee = new Employee(position, accountID, Fname, Lname, address, phoneNum);
+            throws SQLException, java.lang.Exception {
+        if (!isDuplicatedID(accountID, "employee")) {
+            account = new Employee(position, accountID, Fname, Lname, address, phoneNum);
+        }
     }
+
     /**
      * Create a customer account
      * @param DL the driver license number of the user
@@ -55,38 +53,36 @@ public class AccountManagement
      * @param phoneNum the phone number of the user
      */
     public void createCustomer(String DL, int accountID, String Fname, String Lname, String address, String phoneNum)
-            throws SQLException,java.lang.Exception
-    {
-        if(!isDuplicatedID(accountID, "customer"))
-	account = new Customer(generateBarcode(), DL, accountID, Fname, Lname, address, phoneNum);
+            throws SQLException, java.lang.Exception {
+        if (!isDuplicatedID(accountID, "customer")) {
+            account = new Customer(generateBarcode(), DL, accountID, Fname, Lname, address, phoneNum);
+        }
     }
+
     /**
-    * Generate membership barcode
-    * @return barcode
-    */
-    private String generateBarcode()
-    {
-	    String barcode = "101";
-	    return barcode;
+     * Generate membership barcode
+     * @return barcode
+     */
+    private String generateBarcode() {
+        String barcode = "101";
+        return barcode;
     }
+
     /**
      * Edit an existing account
      * @param aAccount the account
      * @param accountType the type of the account
      */
-    public void editAccount(Object aAccount, String accountType)
-    {
-        if(accountType.equals("employee"))
-	{	
-		Employee employee = (Employee)aAccount;
-		account = employee;
+    public void editAccount(Object aAccount, String accountType) {
+        if (accountType.equals("employee")) {
+            Employee employee = (Employee) aAccount;
+            account = employee;
+        } else if (accountType.equals("customer")) {
+            Customer customer = (Customer) aAccount;
+            account = customer;
         }
-	else if(accountType.equals("customer"))
-        {
-		Customer customer=(Customer)aAccount;
-		account = customer;
-	}
     }
+
     /**
      * Check if an account id already exists in the database
      * @param ID an account ID
@@ -95,11 +91,10 @@ public class AccountManagement
      * @throws SQLException 
      */
     private boolean isDuplicatedID(int ID, String accountType)
-	throws SQLException,ClassNotFoundException,java.lang.Exception
-    {
+            throws SQLException, ClassNotFoundException, java.lang.Exception {
+        //todo: generate correct query
         String table = accountType, column = accountType + "ID", query = "";
-	
-        return JDBC.update(query)>0;
+        return JDBC.update(query) > 0;
     }
 
     /**
@@ -107,11 +102,10 @@ public class AccountManagement
      * @param loginID the log in ID/username of the account
      * @param password the password of the account
      */
-    public void setLogin(String loginID, String password)
-    {
+    public void setLogin(String loginID, String password) {
         account.setLogin(loginID, password);
     }
-    
+
     /**
      * Set personal information with 4 attributes
      * @param Fname the first name of the user
@@ -119,8 +113,7 @@ public class AccountManagement
      * @param address the address of the user
      * @param phoneNum the phone number of the user
      */
-    public void setPersonalInfo(String Fname, String Lname, String address, String phoneNum)
-    {
+    public void setPersonalInfo(String Fname, String Lname, String address, String phoneNum) {
         account.setPersonalInfo(Fname, Lname, address, phoneNum);
     }
 
@@ -132,44 +125,38 @@ public class AccountManagement
      * @param address the address of the user
      * @param phoneNum the phone number of the user
      */
-    public void setPersonalInfo(String DL, String Fname, String Lname, String address, String phoneNum)
-    {
-		Customer customer = (Customer)account;
-		customer.setPersonalInfo(Fname, Lname, address, phoneNum);
-		customer.setDL(DL);
-		account = customer;
+    public void setPersonalInfo(String DL, String Fname, String Lname, String address, String phoneNum) {
+        Customer customer = (Customer) account;
+        customer.setPersonalInfo(Fname, Lname, address, phoneNum);
+        customer.setDL(DL);
+        account = customer;
     }
 
     /**
      * Change the status of the account (Active/Inactive)
      */
-    public void changeStatus()
-    {
+    public void changeStatus() {
         account.changeStatus();
     }
 
     /**
      * Promote an employee to manager
      */
-    public void promoteEmployee()
-    {
-	Employee employee = (Employee)account;
+    public void promoteEmployee() {
+        Employee employee = (Employee) account;
         employee.setPosition("Manager");
-	account = employee;
+        account = employee;
     }
 
     /**
      * Demote a manager to employee
      */
-    public void demoteManager()
-    {
-	Employee employee = (Employee)account;
+    public void demoteManager() {
+        Employee employee = (Employee) account;
         employee.setPosition("Employee");
-	account = employee;
+        account = employee;
     }
-
     private Account account;
-    private Statement statement;
     private JDBCConnection JDBC;
     private Connection connection;
 }
