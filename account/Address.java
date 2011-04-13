a8c32041dcb4fc089c23445362dccf6f70af4872
pc <petchu@gmail.com>
2011-04-13 01:05:25 -0700

    Finished documenting, implementing, unit testing Address class (see AddressDriver.java for sample program)

--- account/Address.java
+++ account/Address.java
@@ -3,130 +3,267 @@
 package account;
 
 import pos.SQLhelper;
-
+import java.lang.ClassNotFoundException;
+import java.sql.SQLException;
 /**
     This Address holds the info about an address.
 */
 public class Address
 {
-     private int addressID;
+     private int addressID;// this is the tuple's primary key, this is set after the address object is inserted into the db, no human employee should ever have to set this
      private int houseNumber;
      private String streetName;
      private String city;
      private String province;
      private String country;
      private String postalCode;
-     
-     private boolean saved = false;
+
+     private boolean saved; // this is if the info has been saved in the db or not
 
      private SQLhelper mySQLhelper;
-     private static final String addressTable = "address";
-     private static final String addressKey = "addressID";
 
     /**
-     * This is the constructor you use that will auto generate the accountID for you (the next available one)
-     * @throws SQLException if sql error.
-     * @throws ClassNotFoundException if sql errors.
+     * This is the constructor you use that will auto generate the accountID for you (the next available one) after you call saveInDB().
+     @param houseNumber the house number of the address
+     @param streetName the street name of the address
+     @param city the city of the address
+     @param province the province of the address
+     @param country the country of the address
+     @param postalCode the postal code of the address
     */
     public Address(
-    //    int addressID,
         int houseNumber,
         String streetName,
         String city,
         String province,
         String country,
-        String postalCode) throws SQLException, ClassNotFoundException
+        String postalCode)
     {
-	mySQLhelper = new SQLhelper;
-//	this.addressID = addresssID;
-	this.addressID = 1 + mySQLhelper.getTotalNumberOfRows(addressTable, addressKey);
-	this.houseNumber = houseNumber;
-	this.streetName = streetName;
-	this.city = city;
-	this.province = province;
-	this.country = country;
-	this.postalCode = postalCode;
+		this.saved = false; // this is a new address object, it has not been saved in the database.
+		constructAddress(houseNumber, streetName, city, province, country, postalCode);
     }
-    /**
-     * This is the constructor that accepts a accountID
-     */
+
+	/**
+     * This is the constructor you use when you get info from the db and create the object (difference from the other constructor is this takes a addressID, the primary key of the Address Table).
+     @param addressID the primary key of the tuple in the database
+     @param houseNumber the house number of the address
+     @param streetName the street name of the address
+     @param city the city of the address
+     @param province the province of the address
+     @param country the country of the address
+     @param postalCode the postal code of the address
+    */
     public Address(
-
+    	int addressID,
+        int houseNumber,
+        String streetName,
+        String city,
+        String province,
+        String country,
+        String postalCode)
+    {
+		this.addressID = addressID;
+		this.saved = true; // all the info in this constructor was retrieved from the database.
+		constructAddress(houseNumber, streetName, city, province, country, postalCode);
+    }
+    /**
+    	Common Constructor code
+    	@param houseNumber the house number of the address
+		@param streetName the street name of the address
+		@param city the city of the address
+		@param province the province of the address
+		@param country the country of the address
+		@param postalCode the postal code of the address
+    */
+    private void constructAddress(
+    	int houseNumber,
+        String streetName,
+        String city,
+        String province,
+        String country,
+        String postalCode)
+    {
+		this.houseNumber = houseNumber;
+		this.streetName = streetName;
+		this.city = city;
+		this.province = province;
+		this.country = country;
+		this.postalCode = postalCode;
+		mySQLhelper = new SQLhelper();
+	}
 
 
     /**
-     * Saves this Address in the DB
-     * @return 
+     * This updates the house number of the address; the address should be saved before doing updates to the object.
+     * @param houseNumber the house number of the address
+     * @throws ClassNotFoundException if JDBC driver is not in CLASSPATH
+     * @throws SQLException if if a database access error occurs
+     * @throws IllegalStateException if info is not in the database yet, so what are you trying to update?
      */
-    public void saveInDB() throws IllegalStateException, SQLException, ClassNotFoundException
+    public void updateHouseNumber(int houseNumber) throws IllegalStateException, SQLException, ClassNotFoundException
     {
-	if (saved == true)
-	    throw new IllegalStateException("Address already saved");
-	mySQLhelper.insertAddressTable(this);
-	saved = true;
+		check("House number");
+		this.houseNumber = houseNumber;
+		mySQLhelper.updateAddress(this);
     }
 
+	/**
+     * This updates the house number of the address; the address should be saved before doing updates to the object.
+     * @param streetName the street name of the address
+     * @throws ClassNotFoundException if JDBC driver is not in CLASSPATH
+     * @throws SQLException if if a database access error occurs
+     * @throws IllegalStateException if info is not in the database yet, so what are you trying to update?
+     */
+    public void updateStreetName(String streetName) throws IllegalStateException, SQLException, ClassNotFoundException
+    {
+		check("Street name");
+		this.streetName = streetName;
+		mySQLhelper.updateAddress(this);
+    }
+    
+    /**
+     * This updates the house number of the address; the address should be saved before doing updates to the object.
+     * @param city the city of the address
+     * @throws ClassNotFoundException if JDBC driver is not in CLASSPATH
+     * @throws SQLException if if a database access error occurs
+     * @throws IllegalStateException if info is not in the database yet, so what are you trying to update?
+     */
+    public void updateCity(String city) throws IllegalStateException, SQLException, ClassNotFoundException
+    {
+		check("City");
+		this.city = city;
+		mySQLhelper.updateAddress(this);
+    }
+    
+    /**
+     * This updates the house number of the address; the address should be saved before doing updates to the object.
+     * @param province the province of the address
+     * @throws ClassNotFoundException if JDBC driver is not in CLASSPATH
+     * @throws SQLException if if a database access error occurs
+     * @throws IllegalStateException if info is not in the database yet, so what are you trying to update?
+     */
+    public void updateProvince(String province) throws IllegalStateException, SQLException, ClassNotFoundException
+    {
+		check("Province");
+		this.province = province;
+		mySQLhelper.updateAddress(this);
+    }
+    
+    /**
+     * This updates the house number of the address; the address should be saved before doing updates to the object.
+     * @param country the country of the address
+     * @throws ClassNotFoundException if JDBC driver is not in CLASSPATH
+     * @throws SQLException if if a database access error occurs
+     * @throws IllegalStateException if info is not in the database yet, so what are you trying to update?
+     */
+    public void updateCountry(String country) throws IllegalStateException, SQLException, ClassNotFoundException
+    {
+		check("Country");
+		this.country = country;
+		mySQLhelper.updateAddress(this);
+    }
+    
+    /**
+    	Updates the postal code for the Address; the address should be saved before doing updates to the object.
+    	@param postalCode the postal code of the address
+    	@throws ClassNotFoundException if JDBC driver is not in CLASSPATH
+    	@throws SQLException if if a database access error occurs
+    	@throws IllegalStateException if info is not in the database yet, so what are you trying to update?
+    */
+    public void updatePostalCode(String postalCode) throws IllegalStateException, SQLException, ClassNotFoundException
+    {
+		check("Postal code");
+		this.postalCode = postalCode;
+		mySQLhelper.updateAddress(this);
+    }
+    
+    /**
+		Checks if this Address is saved.
+		@param theVariable the variable name that is wanting to be changed
+	*/
+	private void check(String theVariable) throws IllegalStateException
+	{
+		if (saved == false)
+			throw new IllegalStateException(theVariable + " can not be updated because the address is not in the database");
+	}
+
+
+	
+	/**
+		This returns a Address object given a attribute (the primary key)
+		@param addressID the primary key of the tuple
+		@return the Address object asscociated with the addressID
+	*/
     public Address getAddress(int addressID) throws IllegalStateException, SQLException, ClassNotFoundException
     {
-	return mySQLhelper.getAddress(int addresssID);
+		return mySQLhelper.getAddress(addressID);
     }
 
-    /**
-     * the address sound be saved before doing updates to the object.
+	/**
+     * Saves this Address in the DB (maybe this should be a private method and be called from the constructor, but if it is in the constructor then you can't instantiate this object if a db connection isn't available).
+     * @return  the addressID (the primary key) of the address
      */
-    public void setHouseNumber(String houseNumber) throws IllegalStateException, SQLException, ClassNotFoundException
+    public int saveInDB() throws IllegalStateException, SQLException, ClassNotFoundException
     {
-	if (saved == false)
-	    throws new IllegalStateException("Address can not be updated because it is not in the database");
-	this.houseNumber = houseNumber;
-	mySQLhelper.updateAddress(this);
+		if (saved == true)
+			throw new IllegalStateException("Address already saved");
+		int addressID = mySQLhelper.insertAddressTable(this);
+		saved = true;
+		return addressID;
     }
-
-    public void setStreetName(String streetName) throws IllegalStateException, SQLException, ClassNotFoundException
-    {
-	if (saved == false)
-	    throws new IllegalStateException("Street name can not be updated because it is not in the database");
-	this.streetName = streetName;
-	mySQLhelper.updateAddress(this);
-    }
-    public void setCity(String city) throws IllegalStateException, SQLException, ClassNotFoundException
-    {
-	if (saved == false)
-	    throws new IllegalStateException("City can not be updated because it is not in the database");
-	this.city = city;
-	mySQLhelper.updateAddress(this);
-    }
-    public void setProvince(String province) throws IllegalStateException, SQLException, ClassNotFoundException
-    {
-	if (saved == false)
-	    throws new IllegalStateException("Province can not be updated because it is not in the database");
-	this.province = province;
-	mySQLhelper.updateAddress(this);
-    }
-    public void setCountry(String country) throws IllegalStateException, SQLException, ClassNotFoundException
-    {
-	if (saved == false)
-	    throws new IllegalStateException("Country can not be updated because it is not in the database");
-	this.country = country;
-	mySQLhelper.updateAddress(this);
-    }
-    public void setPostalCode(String postalCode) throws IllegalStateException, SQLException, ClassNotFoundException
-    {
-	if (saved == false)
-	    throws new IllegalStateException("Postal code can not be saved because it is not in the database");
-	this.postalCode = postalCode;
-	mySQLhelper.updateAddress(this);
-    }
-
-
-
-
     
-    public String getPostalCode() { return postalCode; }
-    public int getAddressID() { return addressID; }
+	// getter methods
+   
+    
+    /**
+    	Gets the primary key of the Address object.
+    	@return the primary key of the row this address is in the database
+    	@throws IllegalStateException if the address has not been saved in the database yet
+    */
+    public int getAddressID() throws IllegalStateException
+    {
+    	if (saved == false)
+    		throw new IllegalStateException("The Address has not been saved in the database");
+    	return addressID;
+    }
+    
+    /**
+    	Gets the house number of the Address.
+    	@return the house number of the Address
+    */
     public int getHouseNumber() { return houseNumber; }
+    
+    /**
+    	Gets the street name of the Address.
+    	@return the street name of the address
+    */
     public String getStreetName() { return streetName; }
+    /**
+    	Gets the city of the address.
+    	@return the city of the address
+    */
     public String getCity() { return city; }
+    /**
+    	Gets the province of the address.
+    	@return the province of the address
+    */
     public String getProvince() { return province; }
-    public String getCountry() { return postalCode; }
+    /**
+    	Gets the country of the address.
+    	@return the country of the address
+    */
+    public String getCountry() { return country; }
+    /**
+    	Gets the postal code of the address.
+    	@return the postal code of the address
+    */
+    public String getPostalCode() { return postalCode; }
+    /**
+    	A string representation of the Address.
+    	@return the address
+    */
+    public String toString()
+    {
+    	return  houseNumber + " " + streetName + ", " + city + ", " + province + ", " + postalCode + ", " + country;
+    }
 }
--- pos/SQLhelper.java
+++ pos/SQLhelper.java
@@ -1,11 +1,7 @@
-/**
-	This is a helper object for TransactionManager
-*/
 
-
-//import pos.Transaction;
 package pos;
-
+//import pos.Transaction;
+import account.Address;
 
 // sql stuff
 import java.lang.ClassNotFoundException;
@@ -18,14 +14,22 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.util.Date;
 
+/**
+	Helper class for my stuff; given a Object (Address or Transaction), save it in the database; given a primary key of a row, get that Object (Address).
+	@author Peter
+*/
 public class SQLhelper
-{
-	// constant for table name
-	// The string variable is the Class name, the value is the db table name
-	public static final String Transaction = "invoice";
-	public static final String GeneralMovie = "videoInfo";
-
-
+{	
+	private static final boolean DEBUG_MODE = true;
+	
+	// constants for table names and their primary keys
+	// the variable names are the class diagram names
+	public static final String TRANSACTION_TABLE_NAME = "invoice";
+	public static final String TRANSACTION_TABLE_PK = "invoiceID";
+	public static final String GENERALMOVIE_TABLE_NAME = "videoInfo";
+	public static final String GENERALMOVIE_TABLE_PK = "InfoID";
+	public static final String ADDRESS_TABLE_NAME = "address";
+	public static final String ADDRESS_TABLE_PK = "addressID";
 
 	private Statement statement;
 	private PreparedStatement pstatement;
@@ -34,21 +38,132 @@ public class SQLhelper
 	
 	/**
 		Gets the total number of rows of a table.
-		@param table the table name.
-		@param key the primary key of the table.
-		@return the total number of invoices stored in the db.
+		@param table the table name
+		@param key the primary key of the table
+		@return the total number of rows of a table, when given its primary key
+		@throws SQLException if a sql error.
+		@throws ClassNotFoundException if connection to db error
 	*/
 	public int getTotalNumberOfRows(String table, String key) throws SQLException, ClassNotFoundException
 	{
 		int total = 0;
-		initDB();
+		setupStatement();
 		ResultSet resultSet = statement.executeQuery("SELECT COUNT(" + key + ") FROM " + table);
 		while (resultSet.next())
 		{
 			total = Integer.parseInt(resultSet.getString(1));
 		}
-		connection.close();
+		closeConnection();
 		return total;
+	}
+	
+	
+	/**
+	 * Method to insert a Address into the db
+	 * @param address the address to save
+	 * @return the addressID (primary key of the tuple) of the address is returned
+	 */
+	public int insertAddressTable(Address address) throws IllegalStateException, SQLException, ClassNotFoundException, NumberFormatException
+	{
+	    int addressID = 1 + getTotalNumberOfRows(ADDRESS_TABLE_NAME, ADDRESS_TABLE_PK);
+	    int houseNumber = address.getHouseNumber();
+	    String streetName = address.getStreetName();
+	    String city = address.getCity();
+	    String province = address.getProvince();
+	    String country = address.getCountry();
+	    String postalCode = address.getPostalCode();
+
+	    String queryString = "INSERT INTO " + ADDRESS_TABLE_NAME + " ("
+		+ "addressID,"
+		+ "houseNumber,"
+		+ "streetName,"
+		+ "city,"
+		+ "province,"
+		+ "country,"
+		+ "postalCode"
+		+ ") VALUES (?, ?, ?, ?, ?, ?, ?)";
+		
+	    setupPreparedStatement(queryString);
+	    pstatement.setInt(1, addressID);
+	    pstatement.setInt(2, houseNumber);
+	    pstatement.setString(3, streetName);
+	    pstatement.setString(4, city);
+	    pstatement.setString(5, province);
+	    pstatement.setString(6, country);
+	    pstatement.setString(7, postalCode);
+	    pstatement.executeUpdate();
+	    closeConnection();
+	    
+	    return addressID;
+	}
+	/**
+		Method to update info in the database of a Address
+		@throws ClassNotFoundException if JDBC driver is not in CLASSPATH
+		@throws SQLException if a database access error occurs or this method is called on a closed connection
+	*/
+	public void updateAddress(Address address) throws ClassNotFoundException, SQLException
+	{
+		int addressID = address.getAddressID();
+	    int houseNumber = address.getHouseNumber();
+	    String streetName = address.getStreetName();
+	    String city = address.getCity();
+	    String province = address.getProvince();
+	    String country = address.getCountry();
+	    String postalCode = address.getPostalCode();
+	    
+	    
+	    
+	    String queryString = "UPDATE " + ADDRESS_TABLE_NAME + " SET "
+		+ "houseNumber=?,"	// 1
+		+ "streetName=?,"	// 2
+		+ "city=?,"			// 3
+		+ "province=?,"		// 4
+		+ "country=?,"		// 5
+		+ "postalCode=?"	//6
+		+ " WHERE addressID=" + addressID;
+		
+	    setupPreparedStatement(queryString);
+	    pstatement.setInt(1, houseNumber);
+	    pstatement.setString(2, streetName);
+	    pstatement.setString(3, city);
+	    pstatement.setString(4, province);
+	    pstatement.setString(5, country);
+	    pstatement.setString(6, postalCode);
+	    
+	    pstatement.executeUpdate();
+	    closeConnection();
+	}
+	/**
+		Method to get an Address when given the primary key.
+		@throws ClassNotFoundException if JDBC driver is not in CLASSPATH
+		@throws SQLException if a database access error occurs or this method is called on a closed connection or no results
+	*/
+	public Address getAddress(int addressID) throws ClassNotFoundException, SQLException
+	{
+		String queryString = "SELECT "
+			+ "houseNumber, "
+			+ "streetName, "
+			+ "city, "
+			+ "province, "
+			+ "country, "
+			+ "postalCode"
+			+ " FROM " + ADDRESS_TABLE_NAME + " WHERE addressID=?";
+		
+		setupPreparedStatement(queryString);
+		pstatement.setInt(1, addressID);
+		
+		ResultSet rs = pstatement.executeQuery();
+		
+		rs.next();
+		
+		return new Address(
+			addressID,
+			rs.getInt("houseNumber"),
+			rs.getString("streetName"),
+			rs.getString("city"),
+			rs.getString("province"),
+			rs.getString("country"),
+			rs.getString("postalCode"));
 	}
 	
 	/**
@@ -76,7 +191,7 @@ public class SQLhelper
 		java.text.SimpleDateFormat sdf = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		String currentTime = sdf.format(dateTime).toString();
 
-		String queryString = "INSERT INTO invoice ("
+		String queryString = "INSERT INTO " + TRANSACTION_TABLE_NAME + " ("
 			+ "invoiceID,"
 			+ "paymentMethod,"
 			+ "dateTime,"
@@ -86,7 +201,7 @@ public class SQLhelper
 			+ "storeID) "
 			+ "VALUES (?, ?, ?, ?, ?, ?, ?)";
 			
-		initDB2(queryString);	// this can throw an exception
+		setupPreparedStatement(queryString);	// this can throw an exception
 		
 		pstatement.setInt(1, invoiceID);
 		pstatement.setString(2, paymentMethod);
@@ -126,7 +241,7 @@ public class SQLhelper
 
 	{
 		// get info to insert into table
-		int infoID = this.getTotalNumberOfRows("videoInfo", "InfoID") + 1;
+		int infoID = 1 + this.getTotalNumberOfRows("videoInfo", "InfoID");
 		String description = "description goes here";
 		String genre = "genre goes here";
 		String producer = "producer goes here";
@@ -157,7 +272,7 @@ public class SQLhelper
 			+ "director) "
 			+ "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 			
-		initDB2(queryString);	// this can throw an exception
+		setupPreparedStatement(queryString);	// this can throw an exception
 		
 		pstatement.setInt(1, infoID);
 		pstatement.setString(2, description);
@@ -172,7 +287,7 @@ public class SQLhelper
 		pstatement.setString(11, director);
 		
 		pstatement.executeUpdate();
-		connection.close();
+		closeConnection();
 		return infoID;
 	}
 	
@@ -188,30 +303,48 @@ public class SQLhelper
 	
 	
 	/**
-		Init connection to the db and setup the PreparedStatement
-		@param sql the sql prepared statement
+		Init connection to the db for a SQL PreparedStatement (remember to close the connection after you are done).
+		@param sql the sql prepared statement query string
+		@throws ClassNotFoundException if JDBC driver is not in CLASSPATH
 		@throws SQLException if a database access error occurs or this method is called on a closed connection
 	*/
-	private void initDB2(String sql) throws SQLException, ClassNotFoundException
+	private void setupPreparedStatement(String sql) throws ClassNotFoundException, SQLException
 	{
-		initDBcommon();
+		openConnection();
 		pstatement = connection.prepareStatement(sql);
 	}
+	
 	/**
-		Init connection to the db and setup the Statement
-		@throws SQLException if a database access error occurs or this method is called on a closed connection.
+		Init connection to the db for a SQL Statement (remember to close the connection after you are done).
+		@throws ClassNotFoundException if JDBC driver is not in CLASSPATH
+		@throws SQLException if a database access error occurs or this method is called on a closed connection
 	*/
-	private void initDB() throws SQLException, ClassNotFoundException
+	private void setupStatement() throws ClassNotFoundException, SQLException
 	{
-		initDBcommon();
-		statement = connection.createStatement(); // this can throw SQLException
+		openConnection();
+		statement = connection.createStatement(); // throws SQLException if a database access error occurs or this method is called on a closed connection
 	}
-	private void initDBcommon() throws SQLException, ClassNotFoundException
+	
+	/**
+		Code that is common to connecting to the db (This opens a connection).
+		@throws ClassNotFoundException if JDBC driver is not in CLASSPATH
+		@throws SQLException if a database access error occurs
+	*/
+	private void openConnection() throws ClassNotFoundException, SQLException
 	{
-		Class.forName("com.mysql.jdbc.Driver");
+		Class.forName("com.mysql.jdbc.Driver"); // can throw ClassNotFoundException
 		String url = "jdbc:mysql://174.132.159.251:3306/kpoirier_CPSC2301?user=kpoirier_User&password=foobar";
 //		String url = "jdbc:mysql://192.168.0.16:3306/storedb?user=videoStore&password=over9000";
 		connection = DriverManager.getConnection(url); // this can throw SQLException
+	}
+	
+	/**
+		Closes a connection to the db server
+		@throws SQLException if a database access error occurs
+	*/
+	private void closeConnection() throws SQLException
+	{
+		connection.close();
 	}
 	
 }
