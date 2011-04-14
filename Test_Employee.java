9543d0ecd98f44b54e2c402f90f9f97541cefbc3
pc <petchu@gmail.com>
2011-04-14 12:28:58 -0700

    Commnented out methods and attributes that have to do with status.

--- Test_Employee.java
+++ Test_Employee.java
@@ -7,17 +7,31 @@
  * @author Peter
  */
 import account.Employee;
+import account.Address;
+import pos.SQLhelper;
+
 public class Test_Employee
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws Throwable
     {
+	SQLhelper mySQLhelper = new mySQLhelper();
+
+	Address empAddress = new Address(223, "Cambie st.", "Vancouver", "B.C.", "Canada", "V6V 1V1");
+
+
+
+
+
+
+
+
 	String position = "slave";
 	int accountID = 5;
 	String firstName = "Tom";
 	String lastName = "Smith";
 	String address = "4321 Electric Ave.";
 	String phoneNum = "604-555-9876";
-	Employee myEmployee = new Employee(position, accountID, firstName, lastName, address, phoneNum);
+	Employee myEmployee = new Employee(position, accountID, firstName, lastName, empAddress, phoneNum);
     }
 
 }
