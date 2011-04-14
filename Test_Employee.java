5977a938da54c0404089ea48ca47b36966abd347
pc <petchu@gmail.com>
2011-04-14 12:40:19 -0700

    Test2 fail

--- Test_Employee.java
+++ Test_Employee.java
@@ -14,7 +14,7 @@ public class Test_Employee
 {
     public static void main(String[] args) throws Throwable
     {
-	SQLhelper mySQLhelper = new mySQLhelper();
+	SQLhelper mySQLhelper = new SQLhelper();
 
 	Address empAddress = new Address(223, "Cambie st.", "Vancouver", "B.C.", "Canada", "V6V 1V1");
 
@@ -29,7 +29,7 @@ public class Test_Employee
 	int accountID = 5;
 	String firstName = "Tom";
 	String lastName = "Smith";
-	String address = "4321 Electric Ave.";
+	//String address = "4321 Electric Ave.";
 	String phoneNum = "604-555-9876";
 	Employee myEmployee = new Employee(position, accountID, firstName, lastName, empAddress, phoneNum);
     }
