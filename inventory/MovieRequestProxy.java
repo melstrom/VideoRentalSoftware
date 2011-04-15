fff11a526015d94bed568eb933c53667f0ae2f17
unknown <cwong22@.(none)>
2011-04-14 21:28:06 -0700

    Proxy rework

---
+++ inventory/MovieRequestProxy.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package inventory;
+
+/**
+ *
+ * @author cwong22
+ */
+public class MovieRequestProxy implements proxy.MovieRequest
+{
+    public MovieRequestProxy()
+    {
+
+    }
+public void addMovieRequest()
+    {
+        String test = "test";
+    }
+}
---
+++ inventory/ReserveMovieProxy.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package inventory;
+
+/**
+ *
+ * @author cwong22
+ */
+public class ReserveMovieProxy implements proxy.ReserveMovie
+{
+    public void reserveMovie()
+    {
+        String test = "test";
+    }
+}
