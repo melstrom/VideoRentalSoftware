2669ed64151208c876eef7b1f3d4c125cccb2dae
unknown <Albert@.(none)>
2011-04-15 00:47:41 -0700

    Added new file

---
+++ ui/util.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ui;
+
+/**
+ *
+ * @author Albert
+ */
+public class util {
+
+    public enum UiMode {
+    Customer, Employee
+}
+
+
+}
