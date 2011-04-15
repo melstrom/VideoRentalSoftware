2be230b7e7792cf9e52c741169a8799580904c5c
Kevin Liu <no_one_knows_649@hotmail.com>
2011-04-15 13:41:54 -0700

    testing check in.... please work... T^T

---
+++ CheckInTest.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import inventory.*;
+imoprt java.sql.*;
+/**
+ *
+ * @author kevin
+ */
+public class CheckInTest
+{
+    public static void main(String[] args)
+            throws SQLException
+    {
+        RentalMovieManagement rentalManager = new RentalMovieManagement();
+        int customerID = 0;
+        String barcode = "678149496420" + "100000011";
+        String newCondition = "broken";
+
+        rentalManager.checkIn(customerID, barcode, newCondition);
+    }
+}
