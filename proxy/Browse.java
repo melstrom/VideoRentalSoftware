f0744a00550166698ecd034ca1abb88b10f006aa
Cary Wong <melstrom@gmail.com>
2011-04-15 17:25:33 -0700

    Proxy rework

---
+++ proxy/Browse.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package proxy;
+
+/**
+ *
+ * @author melstrom
+ */
+public interface Browse {
+    public void browseProxy();
+}
