5b109584a6ea49381b8636e78aa10bd965582c85
unknown <xliu01@.(none)>
2011-04-17 13:27:29 -0700

    fixed unknown bug.... :P

--- authentication/Authentication.java
+++ authentication/Authentication.java
@@ -24,55 +24,70 @@ public class Authentication
             {
                 JDBCConnection conn = new JDBCConnection();
                 conn.getConnection();
-                String command = "SELECT account.phoneNum " +
-                        "FROM employee, account" +
-                        "WHERE employee.accountID = account.accountID" +
-                        "AND employee.employeeID = " + ID + ";";
-                PreparedStatement stat = conn.prepareStatement(command);
-                ResultSet result = stat.executeQuery();
-                if(result.next())
+                try
                 {
-                    String phoneNum = result.getString(1);
-                    String expectPassword = phoneNum.substring(phoneNum.length() - 4);
-                    if(expectPassword.equals(password))
+                    String command = "SELECT account.phoneNum " +
+                            "FROM employee, account" +
+                            "WHERE employee.accountID = account.accountID " +
+                            "AND employee.employeeID = " + ID + ";";
+                    PreparedStatement stat = conn.prepareStatement(command);
+                    ResultSet result = stat.executeQuery();
+                    if(result.next())
                     {
-                        account = Search.getEmployee(Integer.parseInt(ID));
-                        return true;
+                        String phoneNum = result.getString(1);
+                        String expectPassword = phoneNum.substring(phoneNum.length() - 4);
+                        if(expectPassword.equals(password))
+                        {
+                            account = Search.getEmployee(Integer.parseInt(ID));
+                            return true;
+                        }
+                        else
+                            return false;
                     }
                     else
-                        return false;
+                        throw new AuthenticationException("The ID provided cannot be found.");
                 }
-                else
-                    throw new AuthenticationException("The ID provided cannot be found.");
+                finally
+                {
+                    conn.closeConnection();
+                }
             }
             else if(ID.charAt(0) == '5')
             {
                 JDBCConnection conn = new JDBCConnection();
                 conn.getConnection();
-                String command = "SELECT account.phoneNum " +
-                        "FROM customer, account" +
-                        "WHERE customer.accountID = account.accountID" +
-                        "AND customer.customerID = " + ID + ";";
-                PreparedStatement stat = conn.prepareStatement(command);
-                ResultSet result = stat.executeQuery();
-                if(result.next())
+                try
                 {
-                    String phoneNum = result.getString(1);
-                    String expectPassword = phoneNum.substring(phoneNum.length() - 4);
-                    if(expectPassword.equals(password))
+                    String command = "SELECT account.phoneNum " +
+                            "FROM customer, account " +
+                            "WHERE customer.accountID = account.accountID " +
+                            "AND customer.customerID = " + ID + ";";
+                    PreparedStatement stat = conn.prepareStatement(command);
+                    ResultSet result = stat.executeQuery();
+                    if(result.next())
                     {
-                        account = Search.getCustomer(Integer.parseInt(ID));
-                        return true;
+                        String phoneNum = result.getString(1);
+                        String expectPassword = phoneNum.substring(phoneNum.length() - 4);
+                        if(expectPassword.equals(password))
+                        {
+                            account = Search.getCustomer(Integer.parseInt(ID));
+                            return true;
+                        }
+                        else
+                            return false;
                     }
                     else
-                        return false;
+                        throw new AuthenticationException("The ID provided cannot be found.");
                 }
-                else
-                    throw new AuthenticationException("The ID provided cannot be found.");
+                finally
+                {
+                    conn.closeConnection();
+                }
             }
             else
                 throw new AuthenticationException("The ID provided cannot be found.");
         }
+        
         public TypeOfPerson whoAmI()
         {
             if(account != null)
