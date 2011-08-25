89e60b92c6ecad7d8d0ad8423710084787be8d69
Cary Wong <melstrom@gmail.com>
2011-08-25 11:38:50 -0700

    Added CheckOutControl skeleton

---
+++ sales/CheckOutControl.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sales;
+
+/**
+ *
+ * @author melstrom
+ */
+public class CheckOutControl {
+
+}
