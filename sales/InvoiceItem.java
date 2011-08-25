47145f8f56edc4ce5e7db1431b4268080a667a02
Cary Wong <melstrom@gmail.com>
2011-08-25 15:41:07 -0700

    Added fns; not sure of accuracy yet

--- sales/InvoiceItem.java
+++ sales/InvoiceItem.java
@@ -4,6 +4,9 @@ package sales;
  *
  * @author melstrom
  */
-public interface InvoiceItem {
-
+public interface InvoiceItem
+{
+    public String getName();
+    public int getPrice();
+    public void checkout();
 }
